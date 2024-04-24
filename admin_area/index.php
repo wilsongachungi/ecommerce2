b6b8ae599d1f7d8827a6b15ed6940060372dc7e7
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-04-24 10:48:12 +0300

    working on rows

---
+++ admin_area/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid ">
+                <img src="../img/shopping.jpeg" alt="" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">welcome Guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+        
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
