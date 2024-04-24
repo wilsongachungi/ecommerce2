5302f1fa4d7d639e82d1d2b2b7f06ba102e55571
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-04-24 11:20:05 +0300

    working on buttons

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,6 +8,12 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../style.css">
+    <style>
+    .admin_img {
+        width: 100px;
+        object-fit: contain;
+    }
+    </style>
 </head>
 
 <body>
@@ -29,7 +35,25 @@
         <div class="bg-light">
             <h3 class="text-center p-2">Manage Details</h3>
         </div>
-        
+
+        <div class="col-md-12 bg-secondary p-1 d-flex align-item-center">
+            <div class="p-3">
+                <a href=""><img src="../img/apple.jpeg" alt="" class="admin_img"></a>
+                <p class="text-light text-center">Admin name</p>
+            </div>
+            <div class="button text-center">
+                <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Products</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">View Products</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">List Users</a></button>
+                <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+            </div>
+        </div>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
