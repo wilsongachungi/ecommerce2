4f38ea62578ce25a465524eaf9dc64a8a990f23a
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-05-04 04:17:50 +0300

    including ip address

--- functions/common_function.php
+++ functions/common_function.php
@@ -267,5 +267,25 @@ function view_details(){
     }
 }
 }
+
+//get ip address function
+
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+}  
+$ip = getIPAddress();  
+echo 'User Real IP Address - '.$ip;
             
 ?>
